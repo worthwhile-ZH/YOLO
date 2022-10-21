b876c42ab323b17523e3033877832a498977fa45
H-ZHANG <1060096896@qq.com>
2022-10-21 17:29:54 +0800

    before the setting of mysql
    before the setting of mysql

--- yolo/src/pages/index/index.vue
+++ yolo/src/pages/index/index.vue
@@ -1,126 +1,77 @@
 <template>
-  <div @click="clickHandle">
-
-    <div class="userinfo" @click="bindViewTap">
-      <img class="userinfo-avatar" v-if="userInfo.avatarUrl" :src="userInfo.avatarUrl" background-size="cover" />
-      <img class="userinfo-avatar" src="/static/images/user.png" background-size="cover" />
-
-      <div class="userinfo-nickname">
-        <card :text="userInfo.nickName"></card>
-      </div>
+  <div>
+    <div class="show">
+      <div class="mark-text">当前分数</div>
+      <div class="mark">{{mark}}</div>
+    </div>
+    <div class="row">
+      <div class="button right" @click="addMark(1)">+1</div>
+      <div class="button left" @click="addMark(-1)">-1</div>
     </div>
 
-    <div class="usermotto">
-      <div class="user-motto">
-        <card :text="motto"></card>
-      </div>
+    <div class="row">
+      <div class="button right" @click="addMark(5)">+5</div>
+      <div class="button left" @click="addMark(-5)">-5</div>
     </div>
 
-    <form class="form-container">
-      <input type="text" class="form-control" :value="motto" placeholder="v-model" />
-      <input type="text" class="form-control" v-model="motto" placeholder="v-model" />
-      <input type="text" class="form-control" v-model.lazy="motto" placeholder="v-model.lazy" />
-    </form>
-
-    <a href="/pages/counter/main" class="counter">去往Vuex示例页面</a>
-
-    <div class="all">
-        <div class="left">
-        </div>
-        <div class="right">
-        </div>
-    </div>
   </div>
 </template>
 
 <script>
-import card from '@/components/card'
 
 export default {
-  data () {
-    return {
-      motto: 'Hello miniprograme',
-      userInfo: {
-        nickName: 'mpvue',
-        avatarUrl: 'http://mpvue.com/assets/logo.png'
-      }
+  data(){
+    return{
+      mark:0
     }
   },
+  methods:{
+    addMark(add){
+      this.mark = this.mark + add
+      
 
-  components: {
-    card
-  },
-
-  methods: {
-    bindViewTap () {
-      const url = '../logs/main'
-      if (mpvuePlatform === 'wx') {
-        mpvue.switchTab({ url })
-      } else {
-        mpvue.navigateTo({ url })
-      }
-    },
-    clickHandle (ev) {
-      console.log('clickHandle:', ev)
-      // throw {message: 'custom test'}
     }
-  },
-
-  created () {
-    // let app = getApp()
   }
+ 
+
+ 
+
+ 
+
+ 
 }
 </script>
 
-<style scoped>
-.userinfo {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-.userinfo-avatar {
-  width: 128rpx;
-  height: 128rpx;
-  margin: 20rpx;
-  border-radius: 50%;
-}
-
-.userinfo-nickname {
-  color: #aaa;
-}
-
-.usermotto {
-  margin-top: 150px;
-}
-
-.form-control {
-  display: block;
-  padding: 0 12px;
+<style lang="scss">
+.show{
+  text-align: center;
+  height: 266px;
+  background: #ea5149;
   margin-bottom: 5px;
-  border: 1px solid #ccc;
+  color: #ffffff;
+  font-weight: bold;
+  .mark-text{
+    font-size: 20px;
+    padding: 28px;
+  }
+  .mark{
+    font-size: 88px;
+  }
 }
-.all{
-  width:7.5rem;
-  height:1rem;
-  background-color:blue;
-}
-.all:after{
-  display:block;
-  content:'';
-  clear:both;
-}
-.left{
-  float:left;
-  width:3rem;
-  height:1rem;
-  background-color:red;
-}
-
-.right{
-  float:left;
-  width:4.5rem;
-  height:1rem;
-  background-color:green;
-}
+  .row{ margin: 40px 56px;
+  .button{ width: 70px;
+  height: 70px;
+  line-height:70px;
+  border-radius: 20%;
+  border: none;
+  text-align:center;
+  font-size: 25px;
+  color:#FFFFFF;
+  font-weight:bold;
+  }
+  }.right{ background:#EA5149;
+  float: right;
+  }.left{ background:#feb600;
+  margin-right:80px;
+  }
 </style>
